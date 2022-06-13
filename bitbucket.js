0067190c3e7cdad10ee4af719708d1600e08b020
onlinehk <onlinehk@gmail.com>
2022-06-13 13:59:39 +0800

    test

--- bitbucket.js
+++ bitbucket.js
@@ -1,21 +1,21 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
-var cmd = require("node-cmd");
+var bodyParser = require('body-parser');
+var cmd = require('node-cmd');
 var port = 8080;
 
-app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
-app.post("/", function (request, response) {
-  response.send("Hello");
+app.post('/', function (request, response) {
+  response.send('Hello');
   if (request.body.push.changes[0].new.name == "master") {
     console.log(request.body.push.changes[0].new.name);
     console.log(request.body.repository.name);
     cmd.get(
-      "cd /home/git/public_html && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1",
+      'cd /home/git/public_html && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1',
       function (err, data, stderr) {
-        console.log("the current working dir is : ", data);
+        console.log('the current working dir is : ', data)
       }
     );
   }
@@ -23,8 +23,8 @@ app.post("/", function (request, response) {
 
 app.listen(port, function (err) {
   if (err) {
-    return console.log("something bad happened", err);
+    return console.log('something bad happened', err);
   }
 
-  console.log("bitbucket 2 server is listening on " + port);
+  console.log('server is listening on ' + port);
 });
--- github.js
+++ github.js
@@ -1,75 +1,66 @@
-var mysql = require("mysql");
-var express = require("express");
+var mysql = require('mysql');
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
-var cmd = require("node-cmd");
-var crypto = require("crypto");
-
+var bodyParser = require('body-parser');
+var cmd = require('node-cmd');
+var crypto = require('crypto');
 var port = 8080;
 var secret = "githubtest";
-var mysql_host = "localhost";
-var mysql_user = "git_user";
-var mysql_password = "github";
-var mysql_database = "git_db";
 
-app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 function verifyGitHub(req) {
-  if (!req.headers["user-agent"].includes("GitHub-Hookshot")) {
+  if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
     return false;
   }
-  // console.log(req.headers['user-agent']);
-  var hmac = crypto.createHmac("sha1", secret);
-  var digest = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
-  var checksum = req.headers["x-hub-signature"];
+  console.log(req.headers['user-agent']);
+  var hmac = crypto.createHmac('sha1', secret);
+  var digest = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+  var checksum = req.headers['x-hub-signature'];
   if (!checksum || !digest || checksum !== digest) {
-    console.log(
-      `Request body digest (${digest}) did not match ${"x-hub-signature"} (${checksum})`
-    );
+    console.log(`Request body digest (${digest}) did not match ${'x-hub-signature'} (${checksum})`);
     return false;
   } else {
-    // console.log(digest);
+    console.log(digest);
     return true;
   }
-}
+};
 
-app.post("/githubpull", function (request, response) {
-  response.send("Hello");
+app.post('/githubpull', function (request, response) {
+  response.send('Hello');
   if (verifyGitHub(request)) {
     var obj = request.body;
     if (obj.ref == "refs/heads/master") {
       // console.log(obj.repository.name);
 
       var connection = mysql.createConnection({
-        host: mysql_host,
-        user: mysql_user,
-        password: mysql_password,
-        database: mysql_database
+        host: 'localhost',
+        user: 'git_user',
+        password: 'github',
+        database: 'git_db'
       });
 
       connection.connect();
 
-      connection.query(
-        "SELECT * FROM github WHERE name='" + obj.repository.name + "'",
-        function (error, rows) {
-          // if (error) {
-          // 	throw error;
-          // }
-          if (rows.length > 0) {
-            cmd.get(
-              "cd " +
-                rows[0].path +
-                " && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1",
-              function (err, data, stderr) {
-                console.log(data);
-              }
-            );
-          } else {
-            console.log("no db");
-          }
+      connection.query("SELECT * FROM github WHERE name='" + obj.repository.name + "'", function (error, rows) {
+        // if (error) {
+        // 	throw error;
+        // }
+        if (rows.length > 0) {
+          // console.log(rows[0].id);
+
+          cmd.get(
+            'cd ' + rows[0].path + ' && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1',
+            function (err, data, stderr) {
+              console.log(data);
+            }
+          );
+
+        } else {
+          console.log('no db');
         }
-      );
+      });
 
       connection.end();
     }
@@ -78,8 +69,8 @@ app.post("/githubpull", function (request, response) {
 
 app.listen(port, function (err) {
   if (err) {
-    return console.log("something bad happened", err);
+    return console.log('something bad happened', err);
   }
 
-  console.log("github server is listening on " + port);
+  console.log('server is listening on ' + port);
 });
