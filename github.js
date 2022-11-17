220bdcc8629736a5c36358ea0fd3a031253dfccd
onlinehk <onlinehk@gmail.com>
2022-11-17 16:14:56 +0800

    Update github.js

--- github.js
+++ github.js
@@ -72,5 +72,5 @@ app.listen(port, function (err) {
     return console.log('something bad happened', err);
   }
 
-  console.log('the github server is listening on ' + port);
+  console.log('github server is listening on ' + port);
 });
